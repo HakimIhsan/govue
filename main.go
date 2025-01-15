3625a41861e74eb91fb5b91afff63e39633e51c5
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2025-01-15 21:43:13 +0700

    deploy7

--- main.go
+++ main.go
@@ -2,16 +2,15 @@ package handler
 
 import (
 	"govue/routes"
-	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 // Exported handler for Vercel
 func Handler(w http.ResponseWriter, r *http.Request) {
-	router := routes.SetupRouter()
+	routers := router.SetupRouter()
 
 	// Serve the request
-	router.ServeHTTP(w, r)
+	routers.ServeHTTP(w, r)
 }
 
 func main() {
